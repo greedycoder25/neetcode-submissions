b0d29c1f9b3f3a1fe46a8c0968963400a5a2cae0
greedycoder25 <148771834+greedycoder25@users.noreply.github.com>
2026-09-23 21:51:51 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& p, int l) {
+        sort(p.begin(),p.end());
+        int i=0;
+        int j=p.size()-1;
+        int ct=0;
+        while(i<=j){
+            if(p[i]+p[j]<=l){
+                i++;
+            }
+            j--;
+            ct++;
+        }
+        return ct;
+    }
+};
